d41871a9bcb13ece4a2834f1bcaecb58cc2c6a3f
Khwilo Kabaka <khwilowatai@gmail.com>
2023-06-20 16:43:48 +0300

    Initial commit

---
+++ src/test/java/dev/khwilo/mezapay/MezaPayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.khwilo.mezapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MezaPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
